9f74a7bc0bcb661e25538d56a2c09013f891a868
Vinicius Gama <vgama@thoughtworks.com>
2015-06-28 17:20:32 -0300

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+DOTFILES_ROOT=$(pwd)
+
+install_dotfiles () {
+  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -type f \( -iname ".*" ! -iname "*.swp" \) -exec basename {} \;)
+  do
+    dst="$HOME/$src"
+    echo "source $(pwd)/$src"
+    echo "dest $dst"
+    echo 'installing dotfiles'
+    rm -rf "$dst"
+    ln -s "$(pwd)/$src" "$dst"
+  done
+}
+install_dotfiles
+
+. ~/.bashrc
